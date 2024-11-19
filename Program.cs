baf75069f2c02c0c501d41aa0e4a169792d3b488
nel <Recarte11nelson@hotmail.com>
2024-11-19 05:01:47 -0600

    ejemplo solid

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace NoInterfaceSegregation
+{
+    public interface IVehiculo
+    {
+        void Conducir();
+        void Volar();
+        void Navegar();
+    }
+
+    public class Auto : IVehiculo
+    {
+        public void Conducir()
+        {
+            Console.WriteLine("El auto está conduciendo.");
+        }
+
+        public void Volar()
+        {
+            throw new NotImplementedException("El auto no puede volar.");
+        }
+
+        public void Navegar()
+        {
+            throw new NotImplementedException("El auto no puede navegar.");
+        }
+    }
+
+    public class Avion : IVehiculo
+    {
+        public void Conducir()
+        {
+            throw new NotImplementedException("El avión no puede conducir.");
+        }
+
+        public void Volar()
+        {
+            Console.WriteLine("El avión está volando.");
+        }
+
+        public void Navegar()
+        {
+            throw new NotImplementedException("El avión no puede navegar.");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IVehiculo auto = new Auto();
+            auto.Conducir();
+            auto.Volar();
+        }
+    }
+}
